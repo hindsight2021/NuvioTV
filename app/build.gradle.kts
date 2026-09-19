bc8d70c00007e9ac7c3c8665246c15b85930624b
hindsight2021 <54675389+hindsight2021@users.noreply.github.com>
2026-09-19 05:56:43 -0300

    chore(release): bump version to v0.9.4-plus.22 (Build 1080)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ android {
         applicationId = "com.nuvio.tv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1079
-        versionName = "0.9.4-plus.21"
+        versionCode = 1080
+        versionName = "0.9.4-plus.22"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
